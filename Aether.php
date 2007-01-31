f02813188ab36ebfe3b9b84834b2bbfb7345e8de
raymond <raymond@ce662b0d-5328-0410-ba4f-a585d2d1e87e>
2007-01-31 13:43:55 +0000

    Started tinkering with config format. Just commiting it not to lose it
    git-svn-id: svn+ssh://svn.hardware.no/svn/Aether/trunk@4 ce662b0d-5328-0410-ba4f-a585d2d1e87e

---
+++ Aether.php
@@ -0,0 +1,49 @@
+<?php
+/*
+HARDWARE.NO EDITORSETTINGS:
+vim:set tabstop=4:
+vim:set shiftwidth=4:
+vim:set smarttab:
+vim:set expandtab:
+*/
+
+require_once('/home/lib/libDefines.lib.php');
+require_once(AETHER_PATH . 'AetherServiceLocator.php');
+/**
+ * 
+ * Main class for Aether.
+ * Fires up the Aether system and delegates down to
+ * section/subsection that is requested based on the
+ * rules.
+ * 
+ * Created: 2007-01-31
+ * @author Raymond Julin
+ * @package aether
+ */
+
+class Aether {
+    
+    /**
+     * Constructor. 
+     * Parses url, prepares everything
+     *
+     * @access public
+     * @return Aether
+     */
+    public function __construct() {
+        $this->sl = new AetherServiceLocator;
+    }
+    
+    /**
+     * Render aether system
+     * Initialization point. When render() is called
+     * everything in the chain of actions is performed one by one
+     * untill we have a response to serve the user
+     *
+     * @access public
+     * @return string
+     */
+    public function render() {
+    }
+}
+?>
---
+++ deployer.php
@@ -0,0 +1,24 @@
+<?php
+/*
+HARDWARE.NO EDITORSETTINGS:
+vim:set tabstop=4:
+vim:set shiftwidth=4:
+vim:set smarttab:
+vim:set expandtab:
+*/
+
+require_once('/home/lib/libDefines.lib.php');
+require('Aether.php');
+
+/**
+ * 
+ * I_AM_TOO_LAZY_TO_WRITE_A_DESCRIPTION
+ * 
+ * Created: 2007-01-31
+ * @author Raymond Julin
+ * @package
+ */
+
+$aether = new Aether;
+$aether->render();
+?>
