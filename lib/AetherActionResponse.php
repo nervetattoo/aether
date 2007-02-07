9405436ae4c09f1787ba757704f38411512faf9a
raymond <raymond@ce662b0d-5328-0410-ba4f-a585d2d1e87e>
2007-02-07 14:41:44 +0000

    Wrote login module. However it does not store the logged in user information anywhere as of yet
    git-svn-id: svn+ssh://svn.hardware.no/svn/Aether/trunk@44 ce662b0d-5328-0410-ba4f-a585d2d1e87e

---
+++ lib/AetherActionResponse.php
@@ -0,0 +1,61 @@
+<?php
+/*
+HARDWARE.NO EDITORSETTINGS:
+vim:set tabstop=4:
+vim:set shiftwidth=4:
+vim:set smarttab:
+vim:set expandtab:
+*/
+
+require_once('/home/lib/libDefines.lib.php');
+require_once(AETHER_PATH . 'lib/AetherResponse.php');
+
+/**
+ * 
+ * Action response
+ * 
+ * Created: 2007-02-07
+ * @author Raymond Julin
+ * @package aether.lib
+ */
+
+class AetherActionResponse extends AetherResponse {
+    
+    /**
+     * Url to redirect to
+     * @var string
+     */
+    private $url = '';
+    
+    /**
+     * Constructor
+     *
+     * @access public
+     * @return AetherActionResponse
+     * @param string $url
+     */
+    public function __construct($url) {
+        $this->out = $url;
+    }
+    
+    /**
+     * Draw text response. Echoes out the response
+     *
+     * @access public
+     * @return void
+     */
+    public function draw() {
+        header("Location: {$this->url}");
+    }
+    
+    /**
+     * Return instead of echo
+     *
+     * @access public
+     * @return string
+     */
+    public function get() {
+        return $this->url;
+    }
+}
+?>
