54aa4d767f3eff0a73f6f49264b68dfdaf6dce5c
raymond <raymond@ce662b0d-5328-0410-ba4f-a585d2d1e87e>
2007-04-23 11:54:26 +0000

    Copied login/logout modules from prisguide repos, this should be totaly general
    git-svn-id: svn+ssh://svn.hardware.no/svn/Aether/trunk@179 ce662b0d-5328-0410-ba4f-a585d2d1e87e

---
+++ modules/AetherModuleLogin.php
@@ -0,0 +1,88 @@
+<?php
+/*
+HARDWARE.NO EDITORSETTINGS:
+vim:set tabstop=4:
+vim:set shiftwidth=4:
+vim:set smarttab:
+vim:set expandtab:
+*/
+
+require_once('/home/lib/libDefines.lib.php');
+require_once(AETHER_PATH . 'lib/AetherModule.php');
+require_once(AETHER_PATH . 'lib/AetherActionResponse.php');
+require_once(LIB_PATH . 'SessionHandler.lib.php');
+require_once(LIB_PATH . 'user/UserFinder.lib.php');
+require_once(LIB_PATH . 'user/User.lib.php');
+require_once(LIB_PATH . 'user/UserAuthenticator.lib.php');
+
+/**
+ * 
+ * User login
+ * 
+ * Created: 2007-02-07
+ * @author Raymond Julin
+ * @package aether.module
+ */
+
+class AetherModulePriceguideLogin extends AetherModule {
+    
+    /**
+     * Render module
+     *
+     * @access public
+     * @return string
+     */
+    public function render() {
+        $config = $this->sl->fetchCustomObject('aetherConfig');
+        // Log user in if requested
+        if (!empty($_POST['username']) AND !empty($_POST['password'])) {
+            // Find username (validate if user exists)
+            $username = mysql_real_escape_string($_POST['username']);
+            $password = mysql_real_escape_string($_POST['password']);
+            $uf = new UserFinder;
+            $userId = $uf->findByUsername($username);
+            if (is_numeric($userId) AND $userId != 0) {
+                // User exists, now validate password
+                $user = new User($userId);
+                $authenticator = new UserAuthenticator;
+                if ($authenticator->authenticate($user, $password)) {
+                    // Set user to appear as logged in (session data)
+                    $session = $this->sl->fetchCustomObject('session');
+                    $session->set('loggedIn', 1);
+                    $session->set('userId', $userId);
+
+                    /* This is hackish from a to z. First of all
+                     * we shouldnt just replace /login as other names
+                     * could be used. Secondly its wrong to draw() in
+                     * the actual module, this should happen in the
+                     * section
+                     */
+
+                    if ($session->get('wasGoingTo')) 
+                        $location = $session->get('wasGoingTo');
+                    else 
+                        $location = $config->getBase();
+
+                    $response = new AetherActionResponse($location);
+                    $response->draw();
+                }
+                else {
+                    // Wrong password
+                    $error = 'password';
+                }
+            }
+            else {
+                $error = 'username';
+            }
+        }
+        $tpl = $this->sl->getTemplate(99);
+        $tpl->selectTemplate('login');
+        if (isset($error)) {
+            $tpl->setVar('failedAt', $error);
+            $tpl->setVar('username', $username);
+        }
+        $tpl->setVar('urlbase', $config->getBase());
+        return $tpl->returnPage();
+    }
+}
+?>
---
+++ modules/AetherModuleLogout.php
@@ -0,0 +1,51 @@
+<?php
+/*
+HARDWARE.NO EDITORSETTINGS:
+vim:set tabstop=4:
+vim:set shiftwidth=4:
+vim:set smarttab:
+vim:set expandtab:
+*/
+
+require_once('/home/lib/libDefines.lib.php');
+require_once(AETHER_PATH . 'lib/AetherModule.php');
+require_once(AETHER_PATH . 'lib/AetherActionResponse.php');
+require_once(LIB_PATH . 'SessionHandler.lib.php');
+require_once(LIB_PATH . 'user/UserFinder.lib.php');
+require_once(LIB_PATH . 'user/User.lib.php');
+require_once(LIB_PATH . 'user/UserAuthenticator.lib.php');
+
+/**
+ * 
+ * User login
+ * 
+ * Created: 2007-02-07
+ * @author Raymond Julin
+ * @package aether.module
+ */
+
+class AetherModuleLogout extends AetherModule {
+    
+    /**
+     * Render module
+     *
+     * @access public
+     * @return string
+     */
+    public function render() {
+        // Log user in if requested
+        $session = $this->sl->fetchCustomObject('session');
+        $session->close();
+
+        /* This is hackish from a to z. First of all
+         * we shouldnt just replace /login as other names
+         * could be used. Secondly its wrong to draw() in
+         * the actual module, this should happen in the
+         * section
+         */
+        $config = $this->sl->fetchCustomObject('aetherConfig');
+        $response = new AetherActionResponse($config->getBase());
+        $response->draw();
+    }
+}
+?>
