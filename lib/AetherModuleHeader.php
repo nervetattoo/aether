be0b4c000613ae880e0fcd5e0d3a18fdd0c9fec7
raymond <raymond@ce662b0d-5328-0410-ba4f-a585d2d1e87e>
2007-05-16 07:52:43 +0000

    Added a common module for header modules, implements some code every header module most likely needs anyway
    git-svn-id: svn+ssh://svn.hardware.no/svn/Aether/trunk@210 ce662b0d-5328-0410-ba4f-a585d2d1e87e

---
+++ lib/AetherModuleHeader.php
@@ -0,0 +1,56 @@
+<?php // vim:set tabstop=4 shiftwidth=4 smarttab expandtab:
+
+/**
+ * 
+ * Base definition of a header module
+ * Theres quite a bit common code every header type module
+ * would want to implement so its done here.
+ * Remember to extend this class!
+ * 
+ * Created: 2007-05-16
+ * @author Raymond Julin
+ * @package aether.lib
+ */
+
+abstract class AetherModuleHeader extends AetherModule {
+    /**
+     * Apply common variables to the header template
+     * Common variables include:
+     * * pageStylesheets
+     * * pageJavascripts
+     * * urlbase
+     *
+     * @access protected
+     * @return void
+     * @param Template $tpl
+     */
+    protected function applyCommonVariables($tpl) {
+        $config = $this->sl->get('aetherConfig');
+        $options = $config->getOptions();
+
+        $tpl->setVar('urlbase', $config->getBase()); 
+        // Stylesheets. Read from config and code
+        $styles = array();
+        if (array_key_exists('styles', $options)) {
+            $styles = explode(';', $options['styles']);
+        }
+        foreach ($this->sl->getVector('styles') as $style) {
+            $styles[] = $style;
+        }
+        $styles = array_map('trim', $styles);
+        $styles = array_unique($styles);
+        $tpl->setVar('pageStylesheets', $styles);
+        // Scripts
+        $scripts = array();
+        if (array_key_exists('javascripts', $options)) {
+            $scripts = explode(';', $options['javascripts']);
+        }
+        foreach ($this->sl->getVector('javascripts') as $script) {
+            $scripts[] = $script;
+        }
+        $scripts = array_map('trim', $scripts);
+        $scripts = array_unique($scripts);
+        $tpl->setVar('pageJavascripts', $scripts);
+    }
+}
+?>
