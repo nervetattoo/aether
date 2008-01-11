075b527109de5d748a84acc9f4ddc060667ba57a
raymond <raymond@ce662b0d-5328-0410-ba4f-a585d2d1e87e>
2008-01-11 14:41:32 +0000

    Fixed isRoot check as it used isBase
    git-svn-id: svn+ssh://svn.hardware.no/svn/Aether/trunk@283 ce662b0d-5328-0410-ba4f-a585d2d1e87e

--- lib/AetherConfig.php
+++ lib/AetherConfig.php
@@ -259,7 +259,7 @@ class AetherConfig {
                 // Remember the url base if this is it
                 if ($node->hasAttribute('isBase'))
                     $this->urlBase .= $check.'/';
-                if ($node->hasAttribute('isBase'))
+                if ($node->hasAttribute('isRoot'))
                     $this->urlRoot .= $check.'/';
                 return true;
             }
