911610ebb072eca776f2812c7e083eeebffd8fe9
mads <mads@ce662b0d-5328-0410-ba4f-a585d2d1e87e>
2008-08-19 13:24:22 +0000

    added the  array to the magical template globals in aether
    git-svn-id: svn+ssh://svn.hardware.no/svn/Aether/trunk@376 ce662b0d-5328-0410-ba4f-a585d2d1e87e

--- Aether.php
+++ Aether.php
@@ -134,6 +134,7 @@ class Aether {
         $magic['urlVars'] = $config->getUrlVars();
         $magic['requestUri'] = $_SERVER['REQUEST_URI'];
         $magic['domain'] = $_SERVER['SERVER_NAME'];
+        $magic['server'] = $_SERVER;
         if ($_SERVER['SERVER_PORT'] != 80)
             $magic['domain'] .= ":" . $_SERVER['SERVER_PORT'];
         $magic['options'] = $options;
