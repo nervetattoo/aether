f1ccc933296ec3d96f85271f26395a91a1f041e8
raymond <raymond@ce662b0d-5328-0410-ba4f-a585d2d1e87e>
2008-10-12 16:11:12 +0000

    Improved error message in AetherSectionFactory::create() when being called without a section name
    git-svn-id: svn+ssh://svn.hardware.no/svn/Aether/trunk@387 ce662b0d-5328-0410-ba4f-a585d2d1e87e

--- lib/AetherSectionFactory.php
+++ lib/AetherSectionFactory.php
@@ -37,22 +37,30 @@ class AetherSectionFactory {
      * @param AetherServiceLocator $sl
      */
     public static function create($section, AetherServiceLocator $sl) {
-        $section = 'AetherSection' . ucfirst($section);
-        if (!strpos(self::$path, ';'))
-            $paths = array(self::$path);
-        else {
-            $paths = array_map('trim', explode(';', self::$path));
-        }
-        foreach ($paths as $path) {
-            $file = $path . 'sections/' . $section . '.php';
-            if (file_exists($file)) {
-                include($file);
-                $aetherSection = new $section($sl);
-                return $aetherSection;
+        if (!empty($section)) {
+            $section = 'AetherSection' . ucfirst($section);
+            if (!strpos(self::$path, ';'))
+                $paths = array(self::$path);
+            else {
+                $paths = array_map('trim', explode(';', self::$path));
             }
+            foreach ($paths as $path) {
+                $file = $path . 'sections/' . $section . '.php';
+                if (file_exists($file)) {
+                    include($file);
+                    $aetherSection = new $section($sl);
+                    return $aetherSection;
+                }
+            }
+            $pathString = implode(';', $paths);
+            throw new Exception("Failed to locate section [$section] in searchpath: $pathString");
         }
-        $pathString = implode(';', $paths);
-        throw new Exception("Failed to locate section [$section] in searchpath: $pathString");
+        else {
+            throw new Exception('AetherSectionFactory::create() received an 
+                empty $section input causing it to look for AetherSection
+                that rightfully doesnt exist. This means that <section/> 
+                is not supplied for this rule in the configuration file.');
+        }
     }
 }
 ?>
