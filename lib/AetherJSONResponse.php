f03ddcbb5e20b81338d23650653a6ddb8b16d1c3
raymond <raymond@ce662b0d-5328-0410-ba4f-a585d2d1e87e>
2008-10-23 15:05:07 +0000

    Created AetherJSONResponse
    git-svn-id: svn+ssh://svn.hardware.no/svn/Aether/trunk@388 ce662b0d-5328-0410-ba4f-a585d2d1e87e

---
+++ lib/AetherJSONResponse.php
@@ -0,0 +1,61 @@
+<?php // vim:set ts=4 sw=4 et:
+
+require_once('/home/lib/libDefines.lib.php');
+require_once(AETHER_PATH . 'lib/AetherResponse.php');
+
+/**
+ * 
+ * JSON response
+ * 
+ * Created: 2008-10-23
+ * @author Raymond Julin
+ * @package aether.lib
+ */
+
+class AetherJSONResponse extends AetherResponse {
+    
+    /**
+     * Hold text string for output
+     * @var string
+     */
+    private $struct;
+    
+    /**
+     * Hold cached output
+     * @var string
+     */
+    private $out = '';
+    
+    /**
+     * Constructor
+     *
+     * @access public
+     * @return AetherJSONResponse
+     * @param array $structure
+     */
+    public function __construct($structure) {
+        $this->struct = $structure;
+    }
+    
+    /**
+     * Draw text response. Echoes out the response
+     *
+     * @access public
+     * @return void
+     * @param AetherServiceLocator $sl
+     */
+    public function draw($sl) {
+        echo $this->get();
+    }
+    
+    /**
+     * Return instead of echo
+     *
+     * @access public
+     * @return string
+     */
+    public function get() {
+        return json_encode($this->struct);
+    }
+}
+?>
---
+++ lib/tests/test_aetherjsonresponse.php
@@ -0,0 +1,24 @@
+<?php // vim:set ts=4 sw=4 et:
+
+require_once('/home/lib/libDefines.lib.php');
+require_once(LIB_PATH . 'simpletest.php');
+require_once(AETHER_PATH . 'lib/AetherJSONResponse.php');
+
+class testAetherJsonResponse extends UnitTestCase {
+    public function testEnvironment() {
+        $this->assertTrue(class_exists('AetherJSONResponse'));
+    }
+    
+    public function testResponse() {
+        $struct = array('foo'=>'bar',' bar'=>'foo');
+        $res = new AetherJSONResponse($struct);
+        $out = $res->get();
+        $this->assertEqual($out, '{"foo":"bar"," bar":"foo"}');
+    }
+}
+
+if (testRunMode(__FILE__) == SINGLE) {
+    $test = new testAetherJsonResponse();
+    $test->run($reporter);
+}
+?>
