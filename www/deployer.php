657a64721de49d46bea02923849ae97950f440a1
voldern <voldern@ce662b0d-5328-0410-ba4f-a585d2d1e87e>
2008-12-11 17:09:44 +0000

    Added autloading of classes/interfaces from commonlibs
    git-svn-id: svn+ssh://svn.hardware.no/svn/Aether/trunk@402 ce662b0d-5328-0410-ba4f-a585d2d1e87e

--- www/deployer.php
+++ www/deployer.php
@@ -14,6 +14,58 @@ require(AETHER_PATH . 'Aether.php');
  * @package
  */
 
+/**
+ * Tries to find class/interfaces in commonlibs 
+ * TODO: Make it look for local libs
+ * @param mixed $name Needs to be in CamelCase format
+ * @access protected
+ * @return void
+ */
+function __autoload($name) {
+    $matches = array();
+    $dir = LIB_PATH;
+
+    // Loop over all parts of the class/interface name and find all the parts
+    // of it that refers to folder or file names in /home/lib
+    $matches = preg_split('/([A-Z][^A-Z]+)/', $name, -1, PREG_SPLIT_NO_EMPTY |
+        PREG_SPLIT_DELIM_CAPTURE);
+
+    if (!empty($matches)) {
+        $i = 0;
+        foreach ($matches as $match) {
+            // Check if there is a file with this name. Files have precendence
+            // over folders
+            $filenameArray = array_slice($matches, $i);
+
+            // Turn the rest of the array into a string that can be used as a filename
+            $filename = '';
+            foreach ($filenameArray as $fn)
+                $filename = $filename . $fn;
+
+            // Break out of the loop if we found the file
+            $filename = $dir . $filename . '.php';
+            if (file_exists($filename)) {
+                $filePath = $filename;
+                break;
+            }
+
+            $match = strtolower($match);
+
+            // If there is a directory with this name add it to the dir path
+            if (file_exists($dir . $match))
+                $dir = $dir . $match . "/";
+            else
+                break;
+
+            $i++;
+        }
+
+        // Require the file
+        if (!empty($filePath))
+            require_once($filePath);
+    }
+}
+
 try {
     $aether = new Aether();
     $aether->render();
