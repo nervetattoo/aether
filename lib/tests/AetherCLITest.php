0166ca0d23bb61140da5215448a4348340cfd840
raymond <raymond@ce662b0d-5328-0410-ba4f-a585d2d1e87e>
2009-02-17 21:53:54 +0000

    Corrected path in AetherCLITest for file include to avoid some errors in build
    git-svn-id: svn+ssh://svn.hardware.no/svn/Aether/trunk@436 ce662b0d-5328-0410-ba4f-a585d2d1e87e

--- lib/tests/AetherCLITest.php
+++ lib/tests/AetherCLITest.php
@@ -1,9 +1,8 @@
 <?php // vim:set ts=4 sw=4 et:
 
 require_once('PHPUnit/Framework.php');
-//require_once('../SMSActivation.php');
-//require_once(AETHER_PATH . 'lib/AetherCLI.php');
-require_once('../AetherCLI.php');
+require_once('/home/lib/libDefines.lib.php');
+require_once(AETHER_PATH . 'lib/AetherCLI.php');
 
 /**
  * 
