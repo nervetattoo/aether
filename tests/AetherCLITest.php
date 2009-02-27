85e68b419d7372892bfd932753a2bbd81a12a55f
Raymond Julin <raymond@europa.hardware.no>
2009-02-27 02:04:54 +0100

    Introduced an error to test integrity failing

--- tests/AetherCLITest.php
+++ tests/AetherCLITest.php
@@ -120,5 +120,8 @@ class AetherCLITest extends PHPUnit_Framework_TestCase {
         $this->assertTrue($t2 > $ti);
     }
     
+    public function testFail() {
+        $this->assertTrue(false);
+    }
 }
 ?>
