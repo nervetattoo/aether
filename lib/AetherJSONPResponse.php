b796d80f85d8d73515cf7ecbe6fa1b7683110d0a
Mads Erik Forberg <mads@devel.apps8.hardware.no.webdealhosting.com>
2009-03-18 14:54:24 +0100

    added AetherJSONP-response for crossdomain JSON-requests

---
+++ lib/AetherJSONPResponse.php
@@ -0,0 +1,65 @@
+<?php // vim:set ts=4 sw=4 et:
+
+require_once('/home/lib/libDefines.lib.php');
+require_once(AETHER_PATH . 'lib/AetherResponse.php');
+
+/**
+ * 
+ * JSON response
+ * 
+ * Created: 2009-03-13
+ * @author Mads Erik Forberg
+ * @package aether.lib
+ */
+
+class AetherJSONPResponse extends AetherResponse {
+    
+    /**
+     * Hold text string for output
+     * @var string
+     */
+    private $struct;
+    
+    /**
+     * Hold cached output
+     * @var string
+     */
+    private $out = '';
+
+    private $callback;
+    
+    /**
+     * Constructor
+     *
+     * @access public
+     * @return AetherJSONResponse
+     * @param array $structure
+     */
+    public function __construct($structure, $callback) {
+        $this->struct = $structure;
+        $this->callback = $callback;
+    }
+    
+    /**
+     * Draw text response. Echoes out the response
+     *
+     * @access public
+     * @return void
+     * @param AetherServiceLocator $sl
+     */
+    public function draw($sl) {
+        echo $this->get();
+    }
+    
+    /**
+     * Return instead of echo
+     *
+     * @access public
+     * @return string
+     */
+    public function get() {
+        return $this->callback . "(" . json_encode($this->struct) .")";
+    }
+}
+
+?>
