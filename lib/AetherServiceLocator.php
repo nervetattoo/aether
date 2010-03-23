9d7c2b21b104a2529446df28fa2d3fa4e31f97fd
Mads Erik Forberg <mads@goliath.(none)>
2010-03-23 16:40:48 +0100

    removed exception from SL::set() since we want to overwrite SL vars

--- lib/AetherServiceLocator.php
+++ lib/AetherServiceLocator.php
@@ -82,14 +82,8 @@ class AetherServiceLocator {
      * @param string $name Name to use as lookup for object
      * @param object $object The actual object
      */
-    public function set($name, $object,$force=false) {
-        if (!$this->has($name) OR $force) {
-            $this->custom[$name] = $object;
-        }
-        else {
-            // Throw exception
-            throw new Exception('Object name is already in use ['.$name.']');
-        }
+    public function set($name, $object) {
+        $this->custom[$name] = $object;
     }
     
     /**
