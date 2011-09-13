07e6c730a05e3d8afa58ebf998096c65913b38ed
Simen Graaten <simeng@gmail.com>
2011-09-13 12:50:48 +0200

    Update the start.php script so it works with new code and new template system.  Also turn off cache as default

--- bin/start.php
+++ bin/start.php
@@ -23,6 +23,9 @@ $aether = preg_replace("/bin\/[a-zA-Z0-9.]*$/", "", __FILE__);
 
 // Create directories
 $rootdir = $root . "/" . $name;
+if (file_exists($rootdir)) {
+    exit("{$rootdir} already exists\nExiting\n");
+}
 mkdir($rootdir);
 mkdir($rootdir . "/config");
 mkdir($rootdir . "/www");
@@ -31,9 +34,17 @@ mkdir($rootdir . "/aether");
 mkdir($rootdir . "/modules");
 mkdir($rootdir . "/sections");
 mkdir($rootdir . "/templates");
+mkdir($rootdir . "/templates/compiled");
+echo "NOTICE: Setting " . $rootdir . "/templates/compiled writable for everyone";
+chmod($rootdir . "/templates/compiled", 0777);
 
 // Copy in starter config
 copy($aether . "config/aether.config.xml", $rootdir . "/config/aether.config.xml");
 copy($aether . "www/deployer.php", $rootdir . "/www/deployer.php");
+file_put_contents($rootdir . "/config/test.searchpath.xml", "<config><option name=\"searchpath\">{$aether}</option></config>");
+copy($rootdir . "/config/test.searchpath.xml", $rootdir . "/config/prod.searchpath.xml");
+
+chdir($rootdir . "/config/");
+system("php " . $aether . "bin/generateConfig.php test");
 
 exit("Project [$name] succesfully created\nExiting\n");
